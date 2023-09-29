5ee64130b4f3dd8aabd19e09c47d9d92fae64e59
Marina <korhovama@mail.ru>
2023-09-29 21:28:54 +0300

    Add new Task_54

---
+++ Task_54/Program.cs
@@ -0,0 +1,56 @@
+﻿int RandArr()
+{
+    int rand = new Random().Next(3, 5);
+    return rand;
+}
+
+void RandElement(int[, ] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArr(int[, ] arr)
+{
+     for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void Sorting(int[, ] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1) - 1; j++)
+    {
+        for (int k = j + 1; k < arr.GetLength(1); k++)
+        {
+            if (arr[i, j] < arr[i, k])
+            {
+                int temp = arr[i, j];
+                arr[i, j] = arr[i, k];
+                arr[i, k] = temp;
+            }
+        }
+    }
+}
+}
+
+
+
+int[, ] arr = new int[RandArr(), RandArr()];
+RandElement(arr);
+PrintArr(arr);
+Sorting(arr);
+System.Console.WriteLine();
+PrintArr(arr);
